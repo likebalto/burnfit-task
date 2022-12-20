237b3a819c1362a18f4c0f83bdafb8fc6334f04d
Lucie Im <isr89314@gmail.com>
2022-12-21 05:47:02 +0900

    refactor : profile컴포넌트 추가 및 Home화면 수정

---
+++ src/components/Profile.jsx
@@ -0,0 +1,48 @@
+// mui 컴포넌트
+import React from "react";
+
+// mui
+import { Grid, Typography, Box } from "@mui/material";
+import Avatar from '@mui/material/Avatar';
+import Tooltip from '@mui/material/Tooltip';
+
+// 이미지
+import profileImg from "../images/profileImg.jpg";
+
+
+function Profile() {
+    return (
+        <>
+
+            <Grid container spacing={1}
+                direction="row"
+                justifyContent="space-around"
+                alignItems="center">
+
+                {/* 프로필이미지 자리 */}
+                <Grid item xs={12}>
+                    <Tooltip title="안녕하세요! 2022 구로 청년이룸 온라인 채용박람회 면접 참여자 임새롬입니다!" followCursor>
+                        <Box >
+                            <Avatar
+                                alt="Remy Sharp"
+                                src={profileImg}
+                                sx={{ width: 300, height: 300, mt: 10 }}
+                                variant="rounded" />
+
+                        </Box>
+                    </Tooltip>
+
+
+                </Grid>
+                {/* 프로필내용 자리 */}
+                <Grid item xs={12}>
+                    <Typography variant="h6">제출자 : 임새롬</Typography>
+                    <Typography variant="h6">제출일 : 2022.12.21.수</Typography>
+                </Grid>
+            </Grid>
+        </>
+
+    )
+
+}
+export default Profile;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,36 @@
+import { Link } from 'react-router-dom'
+
+// 컴포넌트
+import Profile from '../components/Profile';
+
+// mui컴포넌트
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+
+
 function Home() {
     return (
         <>
-            <p>여기는 홈</p>
+            <Container maxWidth="sm">
+                {/* 상단바 */}
+                <AppBar>
+                    <Toolbar>
+                        <Typography variant="h6" component="div">
+                            과제제출
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                {/* 프로필 자리 */}
+                <Profile />
+                {/* 과제버튼 자리 */}
+                <Link to="./Task1" style={{ textDecoration: "none" }}>
+                    <Button variant="contained">Task1</Button>
+                </Link>
+            </Container>
         </>
     );
 }
