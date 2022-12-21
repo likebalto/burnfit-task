6ad8a3d55592cea71a7afc013173ac0a34a7f81f
Lucie Im <isr89314@gmail.com>
2022-12-21 14:10:28 +0900

    refactor : 캘린더수정중

--- src/pages/Task2.js
+++ src/pages/Task2.js
@@ -1,7 +1,26 @@
+// 컴포넌트
+import AppBarToHome from '../components/AppBarToHome';
+
+
+// mui컴포넌트
+import Container from '@mui/material/Container';
+
+// ToDO : 
+// - Level 2 : 캘린더 탭에 외부 캘린더 라이브러리를 이용하지 않고 캘린더 컴포넌트를 제작하시오. 캘린더는 아래 이미지와 같은 형태로 월 캘린더로 구현하시오
+//     - 기능 1 : 캘린더에 현재 월을 출력하고 오늘 날짜를 아래 이미지와 같이 구현 하시오.
+//     - 기능 2 : 상단 좌우 화살표 버튼 클릭 시 전월, 익월을 캘린더에 출력 하시오.
+//     - 기능 3 : 캘린더 상에 특정 날짜를 선택하면 해당 날짜에 원을 표시 하시오. 마지막으로 선택된 날짜만 표시해야 함.
+
+
 function Task2() {
     return (
         <>
-            <p>여기는 Task2</p>
+            <Container maxWidth="sm" >
+
+                <AppBarToHome />
+
+            </Container>
+
         </>
     );
 }
--- src/pages/Task3.js
+++ src/pages/Task3.js
@@ -1,7 +1,22 @@
+// 컴포넌트
+import AppBarToHome from '../components/AppBarToHome';
+
+// mui컴포넌트
+import Container from '@mui/material/Container';
+
+// ToDo :
+// - Level 3 : react-native-reanimated, react-native-gesture-handler 라이브러리를 이용
+// - 제스처 이벤트가 발생하면 아래와 같이 캘린더의 형태가 월 캘린더에서 주 캘린더로, 주 캘린더에서 다시 월 캘린더로 변환 가능하도록 구현 하시오
+
 function Task3() {
     return (
         <>
-            <p>여기는 Task3</p>
+            <Container maxWidth="sm" >
+                <AppBarToHome />
+
+
+            </Container>
+
         </>
     );
 }
